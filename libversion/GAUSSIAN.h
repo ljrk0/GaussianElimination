2dbcf790d889d9adb628f7708b39bac452f02b4f
GelberBaron <GelberBaron@users.noreply.github.com>
2014-11-18 22:36:08 +0100

    Create GAUSSIAN.h
    - new visible Header file (other Header file is only for Background, invisible functions)

---
+++ libversion/GAUSSIAN.h
@@ -0,0 +1,22 @@
+#ifndef _GAUSSIAN_H
+#define _GAUSSIAN_H
+
+#ifndef WINAPI
+#define WINAPI  __stdcall
+#endif
+
+#ifndef _GEO
+#define _GEO
+typedef struct GaussianEliminationOutput {
+	double ** ppReducedEcholonForm;
+	double ** ppSMatrix;
+} GAUSSIAN_ELIMINATION_OUTPUT;
+#endif
+
+// Führt den Gauß-Algorithmus zur Berechnung der Treppennormalform durch. 
+// ppMatrix bleibt dabei unverändert. Der Ausgabe-Puffer muss so angepasst sein, dass 
+//   1) in "ppReducedEcholonForm" genügend Speicher reserviert ist zur Aufnahme einer Matrix der gleichen Dimension wie der gegebenen Matrix und 
+//   2) in "ppSMatrix" genügend Speicher reserviert ist zur Aufnahme einer quadratischen Matrix mit der Dimension "iRows | iRows"
+int WINAPI GaussianElimination(double ** ppMatrix, int iRows, int iColumns, GAUSSIAN_ELIMINATION_OUTPUT * pOutputBuffer);
+
+#endif /* _GAUSSIAN_H */
